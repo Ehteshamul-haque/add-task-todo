ccd962238af3fb821e45a9a5483ab56664aac14a
Ehteshamul Haque <ehtesham000@gmail.com>
2021-11-07 15:42:45 +0530

    fixed flex heading overflow

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import "./App.css";
 import ListItem from "./ListItem";
 // import InputForm from "./InputForm";
-import Modal from "./Modal"
+// import Modal from "./Modal"
 function App() {
  
 
   return (
     <>
       <ListItem/>
-      <Modal />
+      {/* <Modal /> */}
     </>
   );
 }
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,19 +1,43 @@
 import React from "react";
-import Button from "./Button"
+import Button from "./Button";
+import './ListItem.css'
 export default function ListItem(props) {
+  const listOfItem = [
+    { name: "Go for Walk", date: "20-12-2018" },
+    { name: "Buy Milk", date: "20-12-2025" },
+    { name: "Buy Vegetables", date: "20-12-2021" },
+    { name: "Go for Walk", date: "20-12-2018" },
+    { name: "Buy Milk", date: "20-12-2025" },
+    { name: "Buy Vegetables", date: "20-12-2021" },
+    { name: "Go for Walk", date: "20-12-2018" },
+    { name: "Buy Milk", date: "20-12-2025" },
+    { name: "Buy Vegetables", date: "20-12-2021" }
+    
+  ];
   return (
     <>
       <div id="container">
+      <div class="fixed-container"></div>
+      <div class="content-wrapper">
         <div className="heading">
           <h1>Task List</h1>
         </div>
         <div className="listItem">
-          <div className="listName">Name</div>
-          <div style={{ fontSize: "13px" }} className="listDate">
-            Date
-          </div>
+          {listOfItem.map((items) => {
+            return (
+            <>
+              <div className="listName">{items.name}</div>
+              <div style={{ fontSize: "13px" }} className="listDate">
+                {items.date}
+              </div>
+              <hr/>
+            </>
+            );
+          })}
         </div>
         <Button />
+        </div>
+        <div class="fixed-container"></div>
       </div>
     </>
   );

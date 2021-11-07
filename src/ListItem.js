c33a08dc5300944ea1ea84ce0ea3ff738c325a58
Ehteshamul Haque <ehtesham000@gmail.com>
2021-11-07 17:18:13 +0530

    fix the default blank list item div container

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,30 +1,29 @@
 import React from "react";
 import Button from "./Button";
-import './ListItem.css'
+import "./ListItem.css";
 export default function ListItem(props) {
-   
   return (
     <>
       <div id="container">
-      <div className="fixed-container"></div>
-      <div className="content-wrapper">
-        <div className="heading">
-          <h1>Task List</h1>
-        </div>
-        <div className="listItem">
+        <div className="fixed-container"></div>
+        <div className="content-wrapper">
+          <div className="heading">
+            <h1>Task List</h1>
+          </div>
+
           {props.listItem.map((items) => {
             return (
-            <div key={items.id}>
-              <div className="listName">{items.name}</div>
-              <div style={{ fontSize: "13px" }} className="listDate">
-                {items.date}
+              <div className="listItem" key={items.id}>
+                  <div className="listName">{items.name}</div>
+                  <div style={{ fontSize: "13px" }} className="listDate">
+                    {items.date}
+                  </div>
+                  <hr />
               </div>
-              <hr/>
-            </div>
             );
           })}
-        </div>
-        <Button showModal={props.showModal}/>
+
+          <Button showModal={props.showModal} />
         </div>
         <div className="fixed-container"></div>
       </div>
